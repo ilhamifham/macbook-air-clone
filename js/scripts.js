eec77c29d038e752b59f211c81c6395721e4e0a7
Ilham Ifham <ilhaamifham@gmail.com>
2025-07-28 23:26:53 +0530

    Added perforamnce section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// NAV
 const header = document.querySelector("header");
 const navButton = document.querySelector("header button");
 const main = document.querySelector("main");
@@ -17,13 +18,19 @@ navButton.addEventListener("click", function () {
 });
 
 document.addEventListener("click", function (event) {
-  if (!header.contains(event.target)) {
+  if (header.classList.contains("nav-open") && !header.contains(event.target)) {
     handleNav();
   }
-})
+});
+
+document.addEventListener("keydown", function (event) {
+  if (header.classList.contains("nav-open") && event.key === "Escape") {
+    handleNav();
+  }
+});
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth >= 834) {
+  if (header.classList.contains("nav-open") && window.innerWidth >= 834) {
     handleNav();
   }
 });
@@ -33,13 +40,21 @@ window.addEventListener("scroll", function () {
     handleNav();
   }
 });
+// ======
 
+// PICTURE GROUPS
 const inputs = document.querySelectorAll(".color-picker input");
-const pictureGroups = [document.querySelectorAll(".gallery .picture-3"), document.querySelectorAll(".gallery .picture-4"), document.querySelectorAll(".gallery .picture-5")];
+const pictureGroups = [
+  document.querySelectorAll(".gallery .picture-3"),
+  document.querySelectorAll(".gallery .picture-4"),
+  document.querySelectorAll(".gallery .picture-5"),
+];
 
 inputs.forEach(function (input, index) {
   input.addEventListener("change", function () {
-    inputs.forEach(function (input) { input.removeAttribute("checked") });
+    inputs.forEach(function (input) {
+      input.removeAttribute("checked");
+    });
 
     inputs[index].setAttribute("checked", "");
 
@@ -56,11 +71,34 @@ inputs.forEach(function (input, index) {
     });
   });
 });
+// ======
 
+// SLIDER
 const nextSlideButton = document.querySelector("#next");
 const prevSlideButton = document.querySelector("#prev");
 const slider = document.querySelector(".design .slider");
 const paragraphs = document.querySelectorAll(".design .slide-images-text p");
+const imageCount = 3;
+
+let isPlaying = true;
+let direction = 1;
+let counter = 0;
+let isClicked = false;
+
+function handleSlider() {
+  slider.style.transform = `translateX(-${100 * counter}%)`;
+}
+
+function handleSlideButton() {
+  if (counter === 0) {
+    prevSlideButton.setAttribute("disabled", "");
+  } else if (counter === imageCount - 1) {
+    nextSlideButton.setAttribute("disabled", "");
+  } else {
+    prevSlideButton.removeAttribute("disabled");
+    nextSlideButton.removeAttribute("disabled");
+  }
+}
 
 function handleParagraphs() {
   paragraphs.forEach(function (paragraph) {
@@ -69,41 +107,55 @@ function handleParagraphs() {
   paragraphs[counter].style.opacity = "100%";
 }
 
-let counter = 0;
-let isClicked = false;
+function handleSlideButtonDebounce() {
+  isClicked = true;
+  setTimeout(function () {
+    isClicked = false;
+  }, 750);
+}
+
+function playAutoSlide() {
+  if (isPlaying) {
+    if (counter === imageCount - 1) direction = -1;
+    if (counter === 0) direction = 1;
+    counter += direction;
+
+    handleSlider();
+    handleSlideButton();
+    handleParagraphs();
+    handleSlideButtonDebounce();
+  }
+}
+
+function pauseAutoSlide() {
+  isPlaying = false;
+  clearInterval(playAutoSlide);
+}
+
+setInterval(playAutoSlide, 3000);
 
 nextSlideButton.addEventListener("click", function () {
+  pauseAutoSlide();
+
   if (isClicked) return;
 
   counter += 1;
-  slider.style.transform = `translateX(-${100 * counter}%)`;
-  prevSlideButton.removeAttribute("disabled");
+
+  handleSlider();
+  handleSlideButton();
   handleParagraphs();
-
-  if (counter === 2) {
-    this.setAttribute("disabled", "");
-  }
-
-  isClicked = true;
-  setTimeout(function () {
-    isClicked = false;
-  }, 1000);
+  handleSlideButtonDebounce();
 });
 
 prevSlideButton.addEventListener("click", function () {
+  pauseAutoSlide();
+
   if (isClicked) return;
 
   counter -= 1;
-  slider.style.transform = `translateX(-${100 * counter}%)`;
-  nextSlideButton.removeAttribute("disabled");
+
+  handleSlider();
+  handleSlideButton();
   handleParagraphs();
-
-  if (counter === 0) {
-    this.setAttribute("disabled", "");
-  }
-
-  isClicked = true;
-  setTimeout(function () {
-    isClicked = false;
-  }, 1000);
+  handleSlideButtonDebounce();
 });
